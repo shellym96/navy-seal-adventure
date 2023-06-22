a131cb33d2492ac70c362fb2d9183441d6076943
shellym96 Maughan <shellymaughan10@gmail.com>
2023-06-22 18:37:11 +0000

    Add first set of Qs with 3 different results

--- run.py
+++ run.py
@@ -10,7 +10,16 @@ if (option == "i"):
 elif (option == "s"): 
     print("The game is beginning... \n")
     name = input("Enter your name here: \n")
-    print("Hello", name "let's begin \n")
+    print("Hello, " + name + " let's begin \n")
+    print("You're being sent for deployment to Afghanistan. Do you leave you're family and risk your life? Y/N \n")
 
-else: 
-    print("Invaild option. \n")
+    choice = input("")
+
+    if(choice == "y"):
+        print("Congratulations, you've been offered to be a Commanding Officer of this mission. Do you accept this position? Y/N \n")
+
+    elif(choice == "n"):
+        print("You have been discharged for not following orders. Return to the home page with the option to play again. \n ")
+
+    else: 
+        print("Invaild option. Please answer questions with y or n only.\n")
