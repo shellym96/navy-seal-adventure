d1cb2a2ce730aa09d54f258907812a985de2990b
shellym96 Maughan <shellymaughan10@gmail.com>
2023-06-22 16:31:05 +0000

    Create welcome message, input for instruc or start

--- run.py
+++ run.py
@@ -1,3 +1,16 @@
-# Your code goes here.
-# You can delete these comments, but do not change the name of this file
-# Write your code to expect a terminal of 80 characters wide and 24 rows high
+#print a welcome message
+print("Welcome to your Navy Seal Adventure!")
+
+print("Choose one of the two options:")
+option = input("Press 'i' for instructions or 's' to start the game.")
+
+if (option == "i"):
+    print("Here is your instructions")
+
+elif (option == "s"): 
+    print("The game is beginning... \n")
+    name = input("Enter your name here: \n")
+    print("Hello", name "let's begin \n")
+
+else: 
+    print("Invaild option. \n")
