e4961cac12736a702408b7bf9099a21126704a8a
shellym96 Maughan <shellymaughan10@gmail.com>
2023-06-25 16:41:29 +0000

    Define final functions and answers for the quiz

--- run.py
+++ run.py
@@ -1,54 +1,33 @@
-"""
-#print a welcome message
-print("Welcome to your Navy Seal Adventure!")
-
-print("Choose one of the two options:")
-option = input("Press 'i' for instructions or 's' to start the game.")
-
-"""
-#explaining the rules of the game by instructions when user presses i on keyboard
-"""
-if (option == "i"):
-    print("Here is your instructions")
-
-
-        terminateOrSafe = input("  ")
-
-        
-
-        evacuateOrFight = input("  ")
-
-        if(evacuateOrFight == "y"):
-            print("")
-
-        elif(evacuateOrFight == "n"):
-            print(" You chose to stay and fight. Taking the right steps to call air-support gives yous an advantage. \n You take out most of the enemy and the air-support does the rest.\n You and you team arrive home injured but safe.\n Although your injuries lead to you having to have both legs amputated which leads to you never return to the Navy.\n You will never again be called Commanding " + name + " again. \n End of game.")
-        elif(terminateOrSafe == "n"):
-            print("Your first act as Commander " + name + ", you have terminated the enemy which is in violation with rules of engagement. \n Do you lie and say they attacked you and your team? Y/N \n")
-
-        
-        elif(acceptPosition == "n"):
-            print("The enemy has came upon your location and the op is compromised. Your commanding officer orders you to terminate the enemy. Do you obey your orders? Y/N \n")
-        else:
-            print("Invaild option. Please answer questions with y or n only.\n")
-
-
-        print("Invaild option. Please answer questions with y or n only.\n")
-
-
-
-def main():
-
-    acceptPosition = input("")
-    terminateOrSafe = input("")
-    """
-
 from simple_term_menu import TerminalMenu 
 
 def end_of_game():
     print("You have been discharged for not following orders. Return to the home page with the option to play again.")
 
 
+
+def retaliation():
+    print("You lie to cover your back and say it was relatiation that made you termiate the enemy. There is an investigation launched and for this reason you have been discharged. GAME OVER.")
+
+def plead_the_fifth():
+    print("Pleading the fifth is definitely saying your guilty without saying your guilty. Straight away you are sent home and fired. You now work in starbucks. GAME OVER!")
+
+    
+def denied_terminate():
+    print("You obyed your orders from your Commander and terminated the enemy. This is in complete violation with the rules of engagement. Do you lie and tell the head officer that your termination of the enemy was purely retaliation?")
+    options = ['1. yes', '2. no']
+    main_menu = TerminalMenu(options)
+    options_index = main_menu.show()
+    options_choice = options[options_index]
+    if options_choice == '1. yes':
+
+        retaliation()
+        
+    else:
+        plead_the_fifth()
+
+
+def denied_go_safely_():
+    print("You disobeyed you're orders to terminate the enemy. In doing so the enemy gain on you and your men, 100 armed soliders come from every angle and attack. Your entire team are killed but you live. You arrive home injured but alive and the guilt eats you. You decide to never return to the Navy. There for GAME OVER.")
 
 def stay_at_compound():
     print("After deciding to stay, you soon realised it was the right decision. After working another 6 months over seas, you were offered a position to be a Commanding  Officer yet again. You return home to your family as a Commanding Officer. CONGRATULATIONS You have won the game!")
@@ -78,7 +57,7 @@ def evacuate_position():
 
 def stay_and_fight():
     print("You chose to stay and fight. Taking the right steps to call air-support gives yous an advantage. You take out most of the enemy and the air-support does the rest. You and you team arrive home injured but safe. Although your injuries lead to you having to have both legs amputated which leads to you never return to the Navy. You will never again be called Commanding again. GAME OVER!")
-
+    
 
 def go_safely():
     print("You chose to let the enemy go safely. They have told their army where you are hiding and 100 armed soldiers are on their way to you right now. Do you yes evacuate position asap or no stay and fight, call air-support for backup!")
@@ -130,10 +109,10 @@ def denied_poistion():
     options_choice = options[options_index]
     if options_choice == '1. yes':
 
-       go_safely()
+       denied_go_safely_()
         
     else:
-        terminate()
+        denied_terminate()
 
 def congratulations_commander():
     print("Congratulations, you've been offered to be the Commanding Officer of this mission. Do you accept this position?")
