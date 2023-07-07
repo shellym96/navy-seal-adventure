eb6279f48d18e8dc1c7a1c3bb2d97caa60e881f2
shellym96 Maughan <shellymaughan10@gmail.com>
2023-07-07 09:30:55 +0000

    Fix indentation of results.py file

--- results.py
+++ results.py
@@ -4,69 +4,87 @@ from colorama import Fore, Back, Style
 
 user_results = {
     "end_of_game":
-    ("You have been discharged for not"
-    " following orders.\n Go back to play again. \n"),
+    (
+        "You have been discharged for not"
+        " following orders.\n Go back to play again. \n"
+    ),
 
     "relatiation":
-    ("You lie to cover your back and say it was "
-    "relatiation that made you termiate "
-    "the enemy.\n There is an investigation launched "
-    "and for this reason you have been \n "
-    "discharged.\n"
-    "GAME OVER! YOU LOSE."),
+    (
+        "You lie to cover your back and say it was "
+        "relatiation that made you termiate "
+        "the enemy.\n There is an investigation launched "
+        "and for this reason you have been \n "
+        "discharged.\n"
+        "GAME OVER! YOU LOSE."
+    ),
 
     "plead_the_fifth":
-    ("Pleading the fifth is definitely saying "
-    "your guilty without saying your guilty.\n "
-    "Straight away you are sent home and fired."
-    "You now work in starbucks.\n"
-    "GAME OVER! YOU LOSE."),
+    (
+        "Pleading the fifth is definitely saying "
+        "your guilty without saying your guilty.\n "
+        "Straight away you are sent home and fired."
+        "You now work in starbucks.\n"
+        "GAME OVER! YOU LOSE."
+    ),
 
     "denied_terminate":
-    ("You disobeyed you're orders to terminate "
-    "the enemy.\n In doing so the enemy gain on you and "
-    "your men.\n 100 armed soliders come from every angle "
-    "and attack.\n Your entire team are killed but you live.\n "
-    "You arrive home injured but alive and the guilt eats "
-    "you.\n You decide to never return to the Navy. \n"
-    "GAME OVER! YOU LOSE."),
+    (
+        "You disobeyed you're orders to terminate "
+        "the enemy.\n In doing so the enemy gain on you and "
+        "your men.\n 100 armed soliders come from every angle "
+        "and attack.\n Your entire team are killed but you live.\n "
+        "You arrive home injured but alive and the guilt eats "
+        "you.\n You decide to never return to the Navy. \n"
+        "GAME OVER! YOU LOSE."
+    ),
 
     "stay_at_compound":
-    ("After deciding to stay, you soon realised "
-    "it was the right decision\n. After working another 6 months "
-    "over seas,\n you were offered a position to be a Commanding "
-    "Officer yet again.\n You return home to your family as "
-    "a Commanding Officer.\n"
-    "CONGRATULATIONS YOU HAVE WON THE GAME!!!"),
+    (
+        "After deciding to stay, you soon realised "
+        "it was the right decision\n. After working another 6 months "
+        "over seas,\n you were offered a position to be a Commanding "
+        "Officer yet again.\n You return home to your family as "
+        "a Commanding Officer.\n"
+        "CONGRATULATIONS YOU HAVE WON THE GAME!!!"
+    ),
 
     "bring_me_home":
-    ("Choosing to go home in spite of being striped "
-    "of your title,\n leads you to then "
-    "be discharged from the Navy.\n "
-    "You now work in Starbucks.\n"
-    "GAME OVER! YOU LOSE."),
+    (
+        "Choosing to go home in spite of being striped "
+        "of your title,\n leads you to then "
+        "be discharged from the Navy.\n "
+        "You now work in Starbucks.\n"
+        "GAME OVER! YOU LOSE."
+    ),
 
     "tell_a_lie":
-    ("One of your team mates have gone to the head of "
-    "operations\n. They said that you attacked first. \n You have been "
-    "given a Military Discharge and sent home.\n"
-    "GAME OVER! YOU LOSE."),
+    (
+        "One of your team mates have gone to the head of "
+        "operations\n. They said that you attacked first. \n You have been "
+        "given a Military Discharge and sent home.\n"
+        "GAME OVER! YOU LOSE."
+    ),
 
     "evacuate_position":
-    ("You evacuate position, 100 men are gaining on you quickly,\n "
-    "you aren't quick enough and you end up having to fight in their "
-    "forests. \n They have the advantage here. "
-    "You being Commander of this op, have failed!\n "
-    "You and your team don't make it.\n"
-    "GAME OVER! YOU LOSE."),
+    (
+        "You evacuate position, 100 men are gaining on you quickly,\n "
+        "you aren't quick enough and you end up having to fight in their "
+        "forests. \n They have the advantage here. "
+        "You being Commander of this op, have failed!\n "
+        "You and your team don't make it.\n"
+        "GAME OVER! YOU LOSE."
+    ),
 
     "stay_and_fight":
-    ("You chose to stay and fight.\n Taking the right steps to "
-    "call air-support gives yous an advantage.\n You take out most "
-    "of the enemy and the air-support does the rest.\n You and you "
-    "team arrive home injured but safe.\n Although your injuries lead "
-    "to you having to have both legs amputated\n which leads to "
-    "you never return to the Navy.\n You will never "
-    "again be called Commander again.\n"
-    "GAME OVER! YOU LOSE.")
+    (
+        "You chose to stay and fight.\n Taking the right steps to "
+        "call air-support gives yous an advantage.\n You take out most "
+        "of the enemy and the air-support does the rest.\n You and you "
+        "team arrive home injured but safe.\n Although your injuries lead "
+        "to you having to have both legs amputated\n which leads to "
+        "you never return to the Navy.\n You will never "
+        "again be called Commander again.\n"
+        "GAME OVER! YOU LOSE."
+    )
 }
